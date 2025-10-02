60d8f1dc513b3c81ccc8948760e7223b7912cc9e
TouhidAhmed0901 <touhidahmed0901@gmail.com>
2025-10-02 14:43:01 +0600

    inverseMod.cpp

---
+++ inverseMod.cpp
@@ -0,0 +1,14 @@
+//binary exponentiation 
+int powerMod(int base, int power, int MOD = mod){
+    int res = 1%MOD;
+    while(power){
+        if(power&1)res = (res*base)%MOD;
+        base = (base*base)%MOD;
+        power = power>>1;
+    }
+    return res;
+}
+// inverseMod
+int inverseMod(int n, int MOD = mod){
+    return powerMod(n, MOD-2, MOD);
+}
