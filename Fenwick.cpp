c67c76ff403a95bfb946c4fe9f6a42ba8084a422
TouhidAhmed0901 <touhidahmed0901@gmail.com>
2026-08-21 17:24:34 +0600

    Create Fenwick.cpp

---
+++ Fenwick.cpp
@@ -0,0 +1,20 @@
+struct Fenwick{
+    int n;
+    vector<long long> bit;
+    Fenwick(int n) : n(n), bit(n + 1, 0) {}
+
+    // Add val to index idx
+    void update(int idx, long long val) {
+        for (; idx <= n; idx += idx & -idx) {
+            bit[idx] += val;
+        }
+    }
+    // Sum of [1 ... idx]
+    long long query(int idx) {
+        long long sum = 0;
+        for (; idx > 0; idx -= idx & -idx) {
+            sum += bit[idx];
+        }
+        return sum;
+    }
+};
